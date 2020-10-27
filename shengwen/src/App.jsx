4d98cc52575dabc6d0b70cfe08529eea49ae8ae7
qingjiuduzhuo <3149087578@qq.com>
2020-10-27 10:55:01 +0800

    add-my

--- shengwen/src/App.jsx
+++ shengwen/src/App.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom"
+import Login from "./login/Login";
+import Home from "./home/Home";
+import Submitted from "./my/Submitted";
+import FollowAndFans from "./my/FollowAndFans";
+import MyPersonalData from "./my/MyPersonalData";
+// import {NavLink} from "react-router-dom"
+// import {NavBar} from "./StyledApp";
+// import home from "./assets/img/home.png"
+// import dynamic from "./assets/img/dynamic.png"
 
 class App extends Component {
   render() {
     return (
       <div>
-        123
+        <Switch>
+          <Route path="/" exact><Home/></Route>
+          <Route path="/login"><Login/></Route>
+          <Route path="/submitted"><Submitted/></Route>
+          <Route path="/my/:tag"><FollowAndFans/></Route>
+          <Route path="/myPersonalData"><MyPersonalData/></Route>
+        </Switch>
       </div>
     );
   }
